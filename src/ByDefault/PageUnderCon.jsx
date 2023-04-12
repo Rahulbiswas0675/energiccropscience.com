f9494c8dc04fe48d2c915fde1a5657ac3c3e7f62
Rahul Biswas <100189999+Rahulbiswas0675@users.noreply.github.com>
2023-04-12 22:42:44 +0530

    change file stucture and create build file and default page

---
+++ src/ByDefault/PageUnderCon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PageUnderCon() {
+    return (
+        <>
+            <h1>www.energiccropscience.com</h1>
+            <h3>This website is currently under maintainance</h3>
+        </>
+    )
+}
+
+export default PageUnderCon
---
+++ src/Database/Database.js
@@ -0,0 +1,63 @@
+
+export const BrandData = {
+    brand_name: "Energiccropscience",
+    brand_logo: "logo.png",
+    brand_tagline: "Somethings",
+}
+
+export const NavbarItems = [
+    {
+        item_id: 1,
+        item_name: "Home",
+        item_icon: "home",
+        item_link: "/"
+    },
+    {
+        item_id: 2,
+        item_name: "Products",
+        item_icon: "products",
+        item_link: "/products"
+    },
+    {
+        item_id: 3,
+        item_name: "About Us",
+        item_icon: "about",
+        item_link: "/about"
+    },
+    {
+        item_id: 4,
+        item_name: "Infrastructure",
+        item_icon: "infrastructure",
+        item_link: "/infrastructure"
+    },
+    {
+        item_id: 5,
+        item_name: "Certificates",
+        item_icon: "certificates",
+        item_link: "/certificates"
+    },
+    {
+        item_id: 6,
+        item_name: "Contact Us",
+        item_icon: "contact",
+        item_link: "/contact"
+    }
+]
+
+export const CompanyInfo = {
+    company_name: "Energiccropscience pvt. ltd.",
+    company_address: {
+        floor : "000",
+        city : "abc",
+        state : "bcd",
+        country : "cde",
+        pin : "111111",
+    },
+    company_email : "rahulbiswas@gmailcom",
+    company_reg_no : "12345667796",
+    company_gst_no : "wertyuiopfgh",
+    company_phn_no : "12345678978",
+    company_helpline : "23456789876",
+    company_ceo : "qwertyui",
+    company_dir : "sdfghjkjh",
+}
