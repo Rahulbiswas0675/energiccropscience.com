e8d87759e3583662a1df3c82eb1e5956bb6e55fc
Rahul Biswas <rahul.b@techpax.in>
2023-06-09 22:11:10 +0530

    y

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import "../../Scss/Contact.scss";
 import call from "../../Image/Logos/call.png";
-import contactinfo from '../../Image/SectionImage/contactus.png';
+import contactinfo from "../../Image/SectionImage/contactus.png";
 import topbackground from "../../Image/SectionImage/contact-top.png";
 import Footer from "../Footer/Footer";
+import email from '../../Image/Logos/email.png';
+import address from '../../Image/Logos/address.png';
+import starts from '../../Image/Logos/Star nimation.png';
+
 function Contact() {
   return (
     <div className="contact-components" id="contact">
@@ -31,50 +35,52 @@ function Contact() {
 
         <div className="contact-mid-div">
           <div className="content">
-
             <div className="left">
-
-              <h3 className="heading">
-                Need to know more
-                <br />
-                on details?
-              </h3>
+              <h3 className="heading">Need to know more</h3>
+              <div className="heading">
+                <img src={starts} alt="start" /> on details?</div>
 
               <div className="contact-details">
                 <div className="item">
                   <img src={call} alt="call" />
                   <div className="details">
                     <h3>Call Us</h3>
-                    <p>Office : +91 6292006000</p>
-                    <p>Support : +91 6292007000</p>
-                    <p>Helpline : +91 6292009000</p>
+                    <div className="point">
+                      <p>Office : +91 6292006000</p>
+                      <p>Support : +91 6292007000</p>
+                      <p>Helpline : +91 6292009000</p>
+                    </div>
                   </div>
                 </div>
 
                 <div className="item">
-                  <img src={call} alt="call" />
+                  <img src={email} alt="call" />
                   <div className="details">
-                    <h3>Call Us</h3>
-                    <p>Office : +91 6292006000</p>
-                    <p>Support : +91 6292007000</p>
-                    <p>Helpline : +91 6292009000</p>
+                    <h3>Email</h3>
+                    <div className="point">
+                      <p>energiccropscience@gmail.com</p>
+                      <p>support@energiccropscience.com</p>
+                    </div>
                   </div>
                 </div>
 
                 <div className="item">
-                  <img src={call} alt="call" />
+                  <img src={address} alt="call" />
                   <div className="details">
-                    <h3>Call Us</h3>
-                    <p>Office : +91 6292006000</p>
-                    <p>Support : +91 6292007000</p>
-                    <p>Helpline : +91 6292009000</p>
+                    <h3>Address</h3>
+                    <div className="point">
+                      <p>
+                        117/B, Hem Chandra Naskar Road <br />
+                        Kolkata, West Benagal <br />
+                        India, 700010
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
 
-              <img src={contactinfo} alt="contact-info" className="contactus"/>
+              <img src={contactinfo} alt="contact-info" className="contactus" />
             </div>
-
 
             <div className="right">
               <div className="form-container">
@@ -85,31 +91,28 @@ function Contact() {
                 </p>
                 <form action="" className="form">
                   <div className="input-grup">
-
                     <div className="input-box">
                       <input type="text" />
-                      <p>error</p>
+                      <p></p>
                     </div>
 
                     <div className="input-box">
                       <input type="text" />
-                      <p>error</p>
+                      <p></p>
                     </div>
-
                   </div>
 
                   <div className="input-box-outer">
                     <input type="text" name="" id="" />
-                    <p>error</p>
+                    <p></p>
                   </div>
 
                   <div className="textarea-box">
                     <textarea name="" id="" cols="30" rows="10"></textarea>
-                    <p>error</p>
+                    <p></p>
                   </div>
 
                   <button>SEND MESSAGE</button>
-
                 </form>
               </div>
             </div>
